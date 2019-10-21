efd13ef6585a585ac331d61e4ba913dd03af5cb9
StudioMaX <studiomax.kz@gmail.com>
2019-10-21 14:35:48 +0600

    Full support for IPv6 interface and Unix sockets

--- src/misc_functions.h
+++ src/misc_functions.h
@@ -1,6 +1,7 @@
 #ifndef MISC_FUNCTIONS__H
 #define MISC_FUNCTIONS__H
 #include <string>
+#include <vector>
 
 int lockRegion(int fd, int type, int whence, int start, int len);
 int32_t strtoint32(const std::string& str);
@@ -9,5 +10,6 @@ std::string inttostr(int i);
 std::string hex_decode(const std::string &in);
 std::string bintohex(const std::string &in);
 std::string trim(const std::string &str);
+std::vector<std::string> split(const std::string &s, char delim);
 
 #endif
